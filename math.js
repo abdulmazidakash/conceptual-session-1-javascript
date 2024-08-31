0ae163539761b78748a045b8843c02ec44c21f20
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-01 00:01:33 +0600

    javascript conceptual session video 2

--- math.js
+++ math.js
@@ -5,7 +5,22 @@ var z = 23;
 var total = x + y + z;
 console.log(total);
 
-var sub = x * y;
+var sub = x - y;
+var multi = x * y;
 var divide = x / y;
 console.log(sub);
 console.log(divide);
+
+var value = divide.toFixed(4);
+// console.log(typeof value);
+
+// var value = parseFloat(divide.toFixed(4));
+// var value = parseInt(divide.toFixed(4));
+console.log(5 + value);
+
+//parseFloat(), parseInt()
+
+var x = 3;
+console.log(x ** 4);
+
+console.log(10 % 3);
