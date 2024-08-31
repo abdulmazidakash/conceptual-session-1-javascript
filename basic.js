4c257cdb2f54e230389ce91b935bc6c0bed35eb7
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-31 20:16:52 +0600

    javascript conceptual session video 1

---
+++ basic.js
@@ -0,0 +1,45 @@
+// console.log(5);
+// console.log('akash');
+// console.log(3.45);
+
+// var me = 'akash';
+// console.log(me);
+// me = 23;
+// console.log(me);
+// me = 52;
+// console.log(me);
+
+var x = 5;
+var x = "5";
+var x = false;
+var x = NaN;
+// console.log(typeof x);
+
+//truthy values => true, 'hello', number
+//falsy values => false, "", 0
+
+var x = 5;
+console.log(!!x);
+
+var x = 'akash';
+console.log(!!x);
+
+var x = 0;
+console.log(!!x);
+
+//variable naming convention 
+//possible
+var Akash;
+var abdul_mazid; //snake case
+var abdulMazid; //camel case
+var AbdulMazid;  //pascal case
+var akash1;
+
+//not possible
+var 98;
+var 2akash;
+var abdul-mazid; 
+var @akash;
+var class; (Can not use any reserved keyword);
+
+
---
+++ math.js
@@ -0,0 +1,11 @@
+var x = 32;
+var y = 34;
+var z = 23;
+
+var total = x + y + z;
+console.log(total);
+
+var sub = x * y;
+var divide = x / y;
+console.log(sub);
+console.log(divide);
